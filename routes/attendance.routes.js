bd091202d71c3ac84c1dabd0b19f40ef78a559fa
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-11-07 02:37:41 +0530

    update

--- routes/attendance.routes.js
+++ routes/attendance.routes.js
@@ -1,19 +1,19 @@
-import { Router } from "express";
-import { getAllAttendaces, getAttendaceByDate  } from "../controllers/attendace.controller.js";
+// import { Router } from "express";
+// import { getAllAttendaces, getAttendaceByDate  } from "../controllers/attendace.controller.js";
 
 
 
-const router = Router()
+// const router = Router()
 
 
-router.get("/" , (req , res)=>{
-    return res.json({
-        message : "This is Attendace Routes!"
-    })
-})
+// router.get("/" , (req , res)=>{
+//     return res.json({
+//         message : "This is Attendace Routes!"
+//     })
+// })
 
-router.get("/all" , getAllAttendaces);
-router.get("/date" , getAttendaceByDate);
+// router.get("/all" , getAllAttendaces);
+// router.get("/date" , getAttendaceByDate);
 
 
-export default router;
+// export default router;
