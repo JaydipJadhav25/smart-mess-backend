f9351834ca43f84a3ef14c18335c54500564c153
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-11-14 13:18:27 +0530

    update sms service

--- service/sms.service.js
+++ service/sms.service.js
@@ -3,18 +3,21 @@ import twilio from "twilio"
 
 
 const accountSid ="AC42296b792fa8bbfa1f3e9c779c4539a6";
-const authToken ="beb24e4be582373b013e7622b0eeb76a";
+const authToken ="41734cdee7c1c9bd734e4efba9376f69";
 const client = twilio(accountSid, authToken);
 
-export async function createMessage(number =7249824513) {
+export async function createMessage(number=7249824513) {
         try {
+
+            console.log("number is : " , number);
             
             const message = await client.messages.create({
-            //   body: `Smart Mess System: Your mess fee has been received successfully. Thank you.`,
-             body : "Smart Mess System: Your fee payment of Rs 3300 has been successfully processed.",
+              body: `Smart Mess System: Your mess fee has been received successfully. Thank you.`,
+            //  body : `Smart Mess System: Your fee payment of Rs 3300 has been successfully processed.`,
                 from: "+16506754792",
-                to: `+91${number}`.trim(),
+                to: `+91${number}`,
             });
+
 
             console.log(message);
             
